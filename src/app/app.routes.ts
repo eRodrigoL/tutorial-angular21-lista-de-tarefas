50c5229883e5d5f1d7f3d0a0755b6f3365b6f852
eRodrigoL <e.rodrigo.l@outlook.com>
2026-03-11 21:34:07 -0300

    build: define rotas da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tarefas',
+    loadComponent: () =>
+      import('@pages/listagem-tarefas/listagem-tarefas').then((m) => m.ListagemTarefas),
+  },
+  {
+    path: 'tarefas/criacao',
+    loadComponent: () =>
+      import('@pages/criacao-tarefa/criacao-tarefa').then((m) => m.CriacaoTarefa),
+  },
+  {
+    path: 'tarefas/edicao/:id',
+    loadComponent: () => import('@pages/edicao-tarefa/edicao-tarefa').then((m) => m.EdicaoTarefa),
+  },
+  {
+    path: '',
+    redirectTo: 'tarefas',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'tarefas',
+  },
+];
